bf21f131b82781fd6e94e0d98b6b5e06501bf05b
Movemaster12 <djiose45@gmail.com>
2026-06-17 17:21:01 +0100

    Reworked downloading dataset, moved to right folder and removed df object for now

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,18 +1,15 @@
-import requests
-import pandas as pd
+import requests, shutil, os.path
 
-#  TODO: Make this into function when working - done!
-def get_data():
-    try:
-        url = "https://github.com/Movemaster12/Real-time-Risk-Intelligence/releases/download/pre-release/creditcard.csv"
-        response = requests.get(url)
-        # Need to download data to a file
-        df = pd.DataFrame(response)
-        return df
-    except Exception as e:
-        print(f"Error occured! {e}")
-        # return None
+def download_data(url = "https://github.com/Movemaster12/Real-time-Risk-Intelligence/releases/download/pre-release/creditcard.csv"):
+    # Download dataset and move to the right folder
+    if not os.path.isfile("data/raw/creditcard.csv"):
+        response = requests.get(url).content
+        file = open("creditcard.csv", "wb")
+        file.write(response)
+        file.close()
+        source = "creditcard.csv"
+        dst = "data/raw/"
+        shutil.move(source, dst)    
 
-
-# get_data()
-
+if __name__ == "__main__":
+    download_data()
